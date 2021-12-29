28abeb3ab194bfe7f6e2b2e18883f5c1c5c70d78
MaksGovor <maksgovruha@gmail.com>
2021-12-29 19:16:04 +0200

    Add basic structure

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from 'src/controller/app.controller';
 import { AppService } from 'src/service/app.service';
+import configuration from 'src/config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      cache: true,
+    }),
+    MongooseModule.forRoot(configuration().mongodb.connectionUrl),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
