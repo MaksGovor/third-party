31d7e71b75d13e24795cd9b85d5d3b8dfa081899
MaksGovor <maksgovruha@gmail.com>
2021-12-29 18:49:26 +0200

    change structure

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from 'src/controller/app.controller';
+import { AppService } from 'src/service/app.service';
 
 @Module({
   imports: [],
